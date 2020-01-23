80ad23c9ab6e57fe3f10f9efddbd27a5b1f1e291
TSR <1694009211@qq.com>
2020-01-23 17:23:57 +0800

    hellowword

---
+++ hellowgit.cpp
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+
+typedef struct node {
+	struct node* lc;
+	struct node* rc;
+}Btree;
+
+int main()
+{
+	printf("hellow git hub ");
+}
